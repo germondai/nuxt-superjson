1776d28a3bd71e2104a2eaa8d0855b4c46828390
germondai <bruno.krocek@gmail.com>
2025-02-01 16:20:39 +0100

    weird error

--- src/runtime/utils/fromSuperJSON.ts
+++ src/runtime/utils/fromSuperJSON.ts
@@ -1,16 +1,15 @@
 import { deserialize } from 'superjson'
-import { createError } from 'nuxt/app'
 
 export const fromSuperJSON = <T>(data: T) => {
   try {
     if (data === undefined || data === null)
-      throw createError('No data provided')
+      throw new Error('No data provided')
 
     if (typeof data === 'string')
       data = JSON.parse(data)
 
     if (!(data && typeof data === 'object' && 'json' in data))
-      throw createError('Invalid data provided')
+      throw new Error('Invalid data provided')
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return deserialize(data as any) as T
